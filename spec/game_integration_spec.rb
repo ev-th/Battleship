08e65d8bfbcefabdbf4fb49ed63a7b04da693333
Evan Thomas <evanthomas0292@gmail.com>
2023-04-23 17:12:54 +0100

    Add Game#place_ship integration tests

--- spec/game_integration_spec.rb
+++ spec/game_integration_spec.rb
@@ -2,41 +2,95 @@ require 'game'
 require 'board'
 require 'ship'
 
-
 describe "Game integration" do
   context "initially" do
-    it "has two player names" do
-
-    end
-
-    xit "has a board associated with each player" do
+    it "has two boards" do
+      board_1 = Board.new([])
+      board_2 = Board.new([])
+      game = Game.new(board_1, board_2)
+      expect(game.current_player_board).to eq board_1
+      expect(game.current_opponent_board).to eq board_2
     end
   end
 
   describe "#place_ship" do
     context "when a ship is placed on valid coordinates of the player board" do
-      xit "updates the board" do
+      it "updates the board" do
+        ship_1 = Ship.new(3)
+        board_1 = Board.new([ship_1])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        game.place_ship(3, 'B3', 'horizontal')
+        
+        expected_grid = [
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: ship_1, hit: false}, {ship: ship_1, hit: false}, {ship: ship_1, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}],
+          [{ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}, {ship: nil, hit: false}]
+        ]
+        expect(board_1.grid).to eq expected_grid
       end
-
-      xit "moves the ship to placed_ships" do
+      
+      it "moves the ship to placed_ships" do
+        ship_1 = Ship.new(3)
+        board_1 = Board.new([ship_1])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        game.place_ship(3, 'B3', 'horizontal')
+        expect(board_1.placed_ships).to eq [ship_1]
       end
-
-      xit "removes the ship from unplaced_ships" do
+      
+      it "removes the ship from unplaced_ships" do
+        ship_1 = Ship.new(3)
+        board_1 = Board.new([ship_1])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        game.place_ship(3, 'B3', 'horizontal')
+        expect(board_1.unplaced_ships).to eq []
       end
     end
     
     context "when a ship is placed out of bounds" do
-      xit "fails" do
+      it "fails" do
+        ship_1 = Ship.new(3)
+        board_1 = Board.new([ship_1])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        expect {
+          game.place_ship(3, 'I6', 'horizontal')
+        }.to raise_error "Some of the ship cannot fit on the board in this position."
       end
     end
-
+    
     context "when a ship is placed on top of another ship on the player board" do
-      xit "fails" do
+      it "fails" do
+        ship_1 = Ship.new(3)
+        ship_2 = Ship.new(4)
+        board_1 = Board.new([ship_1, ship_2])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        game.place_ship(4, "A1", "vertical")
+        expect {
+          game.place_ship(3, 'A3', 'horizontal')
+        }.to raise_error "There is already a ship placed there."
       end
     end
-
+    
     context "when the ship is not available to place" do
-      xit "fails" do
+      it "fails" do
+        ship_1 = Ship.new(3)
+        board_1 = Board.new([ship_1])
+        board_2 = Board.new([])
+        game = Game.new(board_1, board_2)
+        expect {
+          game.place_ship(4, 'A3', 'horizontal')
+        }.to raise_error "There are no ships of this length available to place."
       end
     end
   end
@@ -44,6 +98,7 @@ describe "Game integration" do
   describe "#shoot" do
     context "when passed a coordinate that has not been hit on the opponent's board" do
       xit "records the hit on the opponent's board" do
+        
       end
       
       context "when a ship is found at that coordinate" do
